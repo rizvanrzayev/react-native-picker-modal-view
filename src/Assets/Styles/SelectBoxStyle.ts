a6fa70d13817bd009e7e372294f39405ce651355
Rizvan Rzayev <rizvan@rzayev.ws>
2020-09-03 15:31:18 +0400

    test

--- src/Assets/Styles/SelectBoxStyle.ts
+++ src/Assets/Styles/SelectBoxStyle.ts
@@ -1,21 +1,27 @@
 // Global Imports
-import { StyleSheet, ViewStyle, TextStyle, ImageStyle, View } from 'react-native';
+import {
+	StyleSheet,
+	ViewStyle,
+	TextStyle,
+	ImageStyle,
+	View,
+} from "react-native";
 
 export const SelectBoxStyle = StyleSheet.create({
 	container: {
-		backgroundColor: '#fff',
+		backgroundColor: "red",
 		paddingHorizontal: 10,
 		paddingVertical: 10,
-		position: 'relative',
+		position: "relative",
 		borderWidth: 1,
-		borderColor: '#9E9E9E',
-		justifyContent: 'space-between',
-		flexDirection: 'row',
-		alignContent: 'center',
-		alignItems: 'center',
+		borderColor: "#9E9E9E",
+		justifyContent: "space-between",
+		flexDirection: "row",
+		alignContent: "center",
+		alignItems: "center",
 	} as ViewStyle,
 	chooseText: {
-		fontWeight: '500',
+		fontWeight: "500",
 	} as TextStyle,
 	downBtn: {
 		width: 10,
@@ -25,10 +31,10 @@ export const SelectBoxStyle = StyleSheet.create({
 		marginVertical: 10,
 	} as ViewStyle,
 	disabledBtn: {
-		borderColor: '#ddd',
+		borderColor: "#ddd",
 	} as ViewStyle,
 	disabledTxt: {
-		color: '#ddd',
+		color: "#ddd",
 	} as TextStyle,
 	disabledImage: {
 		opacity: 0.2,
